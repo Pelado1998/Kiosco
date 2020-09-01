1f55d0cd0a7bee21e0759ed1e4700316d752de5d
Pelado1998 <rafaelrodriguezsanz@gmail.com>
2020-09-01 19:59:13 -0300

    Cambios para el ejercicio 1

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -13,5 +13,9 @@ namespace Expert_SRP
         }
         public Double PrecioDulce {get;set;}
         public Double PrecioMasa {get; set;}
+        public Boolean PuedeComprar(Double dinero, String moneda) 
+        {
+            return ConvertirAPesos(dinero, moneda)>= this.PrecioDulce + this.PrecioMasa;
+        }
     }
 }
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -2,19 +2,8 @@ using System;
 
 namespace Expert_SRP
 {
-    // La clase Kiosco cumple con Expert,
-    // porque es el experto con la informacion para verificar si se puede comprar un alfajor o no
-    // La clase Kiosco no comple SRP, 
-    // porque tiene más de una razón posible de cambio. Se encarga de ver si se puede comprar o no, 
-    // pero también es el responsable de convertir la moneda según el tipo de cambio 
-
     public class Kiosco         
     {
-        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
-        {
-            Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
-        }
         private Double ConvertirAPesos(Double dinero, String moneda)
         {
             if (moneda.Equals("U$S")) 
